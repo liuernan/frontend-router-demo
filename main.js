ed632237c17c7d85a2a8cda0059438a032eed205
Jiawei <liujiaweidreamer@gmail.com>
2020-08-09 16:33:30 +0800

    假设有四个页面，分别是 div1、div2、div3、div4，一次在首页展示一个

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const div1 = document.createElement("div");
+div1.innerText = "1";
+const div2 = document.createElement("div");
+div2.innerText = "2";
+const div3 = document.createElement("div");
+div3.innerText = "3";
+const div4 = document.createElement("div");
+div4.innerText = "4";
+
+const app = document.querySelector("#app");
+
+app.append(div1);
