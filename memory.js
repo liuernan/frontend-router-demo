7458081f2b9f471ebffbb9ff8ae9e1910a422fee
Jiawei <liujiaweidreamer@gmail.com>
2020-08-15 21:20:54 +0800

    memory router demo 完成。存在 localStorage 里。单机版。url 没变化。

---
+++ memory.js
@@ -0,0 +1,41 @@
+const div1 = document.createElement("div");
+div1.innerText = "1";
+const div2 = document.createElement("div");
+div2.innerText = "2";
+const div3 = document.createElement("div");
+div3.innerText = "3";
+const div4 = document.createElement("div");
+div4.innerText = "4";
+
+const app = document.querySelector("#app");
+
+const routeTable = {
+  "default": div1,
+  "1": div1,
+  "2": div2,
+  "3": div3,
+  "4": div4
+};
+
+const router = () => {
+  const userPath = window.localStorage.getItem("xxx");
+  console.log(userPath);
+  app.innerHTML = "";
+  app.append(routeTable[userPath] || routeTable.default);
+};
+
+router();
+
+const aTags = document.querySelectorAll("a.route-link");
+
+for (let a of aTags) {
+  a.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const href = a.getAttribute("href");
+    window.localStorage.setItem("xxx", href);
+    router();
+  });
+}
+
+
